f01cb5ac9234acc60de690a86d7a4a2bed86d59d
ellen-zomin <57499060+ellen-zomin@users.noreply.github.com>
2020-04-02 17:32:33 -0400

    idk wtv

---
+++ index.js
@@ -0,0 +1,50 @@
+let string = "string";
+const edis = "Edis";
+let sentence = "Hello " + "Vicky";
+let sentence2 = " and also Angel";
+//let combine = sentence + sentence2;
+combine = `$(sentence)$(sentence2)`; //ES2015
+
+let student1 = "Sandra";
+let student2 = "Kelly";
+let courseName = "AP CSP";
+let teacherName = "Whalen";
+/*const longerSentence =
+    student1 +
+    " and " +
+    student2 +
+    " are amazing and will be taking " +
+    courseName +
+    " next year with " +
+    teacherName;
+
+    console/log(longerSentence);*/
+    /*let longSentence = `${student1} and ${student2} will be taking ${courseName} with ${teacherName} next year`
+    console/log(longSentence);
+*/
+
+////////////////////////////////
+//Numbers Integers
+/*let year = 2020;
+let nextYear= 2020 + 1;
+let past = year - 1;
+let lastYear = year - 1;
+let farFuture = year * 100; */
+////////////////////////////////////
+//Booleans, null, undefined
+/*let trueStatement = true;
+let falseStatement = false;
+
+let undefinedVar; */
+//console.log(undefinedVar); //do this a lot
+//console.log(nonExistent); //null never declared
+//console.log(truuStatement);
+
+///if/ else if statements
+let student = "Karen";
+let age = 17;
+// console.log(student === "Karen"); /// true
+// console.log(student === "karen"); ///false
+// console.log(student == "karen"); ///false
+console.log(age === "17"); 
+console.log(age == "17");
